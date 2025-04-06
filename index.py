aa15532953d3a5dd7c4038c82e9817f940ea9cfe
Trungenial <transytrung0752.work@gmail.com>
2025-04-06 19:07:26 +0700

    clean project

--- index.py
+++
@@ -1,9 +0,0 @@
-import datetime as dt
-import pandas as pd
-
-## 1. Web Scape Using Pandas
-url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-data = pd.read_html(url)
-print(f"We have a {type(data)} with length {len(data)}")
-
-print(data[0].head())
